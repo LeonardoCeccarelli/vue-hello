5ac0c097f23b484adc8ac6d7c11433aeb85fddb6
LeonardoCeccarelli <leo.ceccarelli@tiscali.it>
2021-10-28 14:06:13 +0200

    add variables with vue to apply to DOM

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
 "use strict"
 
+Vue.config.devtools = true;
+
 const myVue = new Vue({
-    el: "#vueInput"
+    el: "#vueInput",
+    data: {
+        username: "Spider-Man",
+        h1Style: "title_text",
+        h1Title: "Benvenuto utente! (Scritta fatta con Vue)",
+        imgStyle: "image",
+        imgUrl: "https://www.smartworld.it/wp-content/uploads/2021/07/spider-man.jpg",
+        imgAlt: "Spider-Man image"
+    }
 })
